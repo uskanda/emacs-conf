9644bf05328f0beaf617c7763ffae6c476a28fbc
Yusuke Kanda <uskanda@gmail.com>
2011-11-18 03:40:20 +0900

    add inf-ruby config

---
+++ conf/conf-inf-ruby.el
@@ -0,0 +1,9 @@
+;;----------------------------------------------------------------------------;;
+;; Inf-ruby Settings
+;;----------------------------------------------------------------------------;;
+(autoload 'run-ruby "inf-ruby"
+  "Run an inferior Ruby process")
+(autoload 'inf-ruby-keys "inf-ruby"
+  "Set local key defs for inf-ruby in ruby-mode")
+(add-hook 'ruby-mode-hook '(lambda () (inf-ruby-keys)))
+;;----------------------------------------------------------------------------;;
