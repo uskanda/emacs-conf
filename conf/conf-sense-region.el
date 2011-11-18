3d8ca0cdfcc9884107533f9fd243bc50f2dcf062
Yusuke Kanda <uskanda@gmail.com>
2011-11-19 04:05:12 +0900

    add sense-region

---
+++ conf/conf-sense-region.el
@@ -0,0 +1,11 @@
+;;----------------------------------------------------------------------------;;
+;; sense-region Settings
+;;   http://Taiyuan.org/elisp/sense-region/
+;; 範囲選択を改良する。
+;; C-SPACE
+;;----------------------------------------------------------------------------;;
+(autoload 'sense-region-on "sense-region"
+  "System to toggle region and rectangle." t nil)
+(sense-region-on)
+;;----------------------------------------------------------------------------;;
+
