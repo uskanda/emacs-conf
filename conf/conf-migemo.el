984a53bf2a2e8e4102c23c3b498620b3c323d218
Yusuke Kanda <uskanda@gmail.com>
2011-11-20 21:05:00 +0900

    add migemo

---
+++ conf/conf-migemo.el
@@ -0,0 +1,12 @@
+(if (executable-find "cmigemo")
+    (progn
+      (require 'migemo)
+      (setq migemo-command "cmigemo")
+      (setq migemo-options '("-q" "--emacs"))
+      (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+      (setq migemo-user-dictionary nil)
+      (setq migemo-regex-dictionary nil)
+      (setq migemo-coding-system 'utf-8-unix)
+      (load-library "migemo")
+      (migemo-init))
+    (message "could not find cmigemo exec, migemo will turn off"))
