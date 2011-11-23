238df366339cbc55110e45770ca6677e9a1f22b3
Yusuke Kanda <uskanda@gmail.com>
2011-11-24 01:56:28 +0900

    check avaliability of Rhino in inf-js

--- conf/conf-inf-js.el
+++ conf/conf-inf-js.el
@@ -1,3 +1,6 @@
-(autoload 'run-js "inf-js" nil t)
-(autoload 'inf-js-keys "inf-js" nil)
-(add-hook 'js2-mode-hook '(lambda () (inf-js-keys)))
+(if (executable-find "rhino")
+    (progn
+      (autoload 'run-js "inf-js" nil t)
+      (autoload 'inf-js-keys "inf-js" nil)
+      (add-hook 'js2-mode-hook '(lambda () (inf-js-keys))))
+    (message "inf-js requires external program Rhino. Emacs could not find rhino, inf-js will turn off"))
